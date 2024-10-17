c80e9144acd5cfbff9d764e8df48d68f885822bc
Brandon Pineda <108766467+BrandonLe06@users.noreply.github.com>
2024-10-17 08:42:11 -0600

    Agregar archivos de proyecto.

---
+++ Control_Horas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Control_Horas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
